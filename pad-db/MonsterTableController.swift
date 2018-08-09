04eaefbc08373ad6281b2a7ae97c2f4161d29738
Rohil Thopu <rohilthopu@gmail.com>
2018-08-09 18:49:32 -0500

    added decoding for more values to pass to segue. need to design a monster ui that isn't shit like the one from padhelper

--- pad-db/MonsterTableController.swift
+++ pad-db/MonsterTableController.swift
@@ -26,10 +26,33 @@ class MonsterTableController: UITableViewController, UISearchControllerDelegate,
     var monstersearch:UISearchController!
     
     struct Monster {
-        var name:String
-        init() {
-            self.name = ""
-        }
+        var activeSkillID:Int?
+        var ancestorID:Int?
+        var attrID:Int?
+        var cardID:Int?
+        var cost:Int?
+        var devoMat1:Int?
+        var devoMat2:Int?
+        var devoMat3:Int?
+        var devoMat4:Int?
+        var devoMat5:Int?
+        var evoMat1:Int?
+        var evoMat2:Int?
+        var evoMat3:Int?
+        var evoMat4:Int?
+        var evoMat5:Int?
+        var maxATK:Int?
+        var maxHP:Int?
+        var maxRCV:Int?
+        var minATK:Int?
+        var minHP:Int?
+        var minRCV:Int?
+        var name:String?
+        var rarity:Int?
+        var subAttrID:Int?
+        var type1:Int?
+        var type2:Int?
+        var type3:Int?
     }
     
     override func viewDidLoad() {
@@ -96,11 +119,12 @@ class MonsterTableController: UITableViewController, UISearchControllerDelegate,
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "monstercell")!
         if(isFiltering()) {
-            cell.textLabel!.text = filteredMonsters[indexPath.row].name
+            cell.textLabel!.text = filteredMonsters[indexPath.row].name!
+            cell.detailTextLabel!.text = String(filteredMonsters[indexPath.row].cardID!)
         }
         else {
-            cell.textLabel!.text = monsters[indexPath.row].name
-            
+            cell.textLabel!.text = monsters[indexPath.row].name!
+            cell.detailTextLabel!.text = String(monsters[indexPath.row].cardID!)
         }
         return cell
     }
@@ -113,9 +137,32 @@ class MonsterTableController: UITableViewController, UISearchControllerDelegate,
                 let json = JSON(parseJSON: data)
                 for item in json.arrayValue {
                     let card = item["card"]
-                    if !card["name"].stringValue.contains("?") && !card["name"].stringValue.contains("*") {
+                    if !card["name"].stringValue.contains("?") && !card["name"].stringValue.contains("*") && !card["name"].stringValue.isEmpty {
                         var monster:Monster = Monster()
+                        monster.activeSkillID = card["active_skill_id"].intValue
+                        monster.ancestorID = card["ancestory_id"].intValue
+                        monster.attrID = card["attr_id"].intValue
+                        monster.cardID = card["card_id"].intValue
+                        monster.cost = card["cost"].intValue
+                        monster.devoMat1 = card["un_evo_mat_1"].intValue
+                        monster.devoMat2 = card["un_evo_mat_2"].intValue
+                        monster.devoMat3 = card["un_evo_mat_3"].intValue
+                        monster.devoMat4 = card["un_evo_mat_4"].intValue
+                        monster.devoMat5 = card["un_evo_mat_5"].intValue
+                        monster.evoMat1 = card["evo_mat_id_1"].intValue
+                        monster.evoMat2 = card["evo_mat_id_2"].intValue
+                        monster.evoMat3 = card["evo_mat_id_3"].intValue
+                        monster.evoMat4 = card["evo_mat_id_4"].intValue
+                        monster.evoMat5 = card["evo_mat_id_5"].intValue
+                        monster.maxATK = card["max_atk"].intValue
+                        monster.maxHP = card["max_hp"].intValue
+                        monster.maxRCV = card["max_rcv"].intValue
+                        monster.minATK = card["min_atk"].intValue
+                        monster.minHP = card["min_hp"].intValue
+                        monster.minRCV = card["min_rcv"].intValue
                         monster.name = card["name"].stringValue
+                        monster.rarity = card["rarity"].intValue
+                        monster.subAttrID = card["sub_attr_id"].intValue
                         monsters.append(monster)
                     }
                 }
@@ -144,7 +191,7 @@ class MonsterTableController: UITableViewController, UISearchControllerDelegate,
     
     func filterContentForSearchText(_ searchText: String, scope: String = "All") {
         
-        filteredMonsters = monsters.filter({$0.name.lowercased().contains(searchText.lowercased())})
+        filteredMonsters = monsters.filter({$0.name!.lowercased().contains(searchText.lowercased())})
         
         monstertable.reloadData()
     }
@@ -155,7 +202,7 @@ class MonsterTableController: UITableViewController, UISearchControllerDelegate,
     
     
     
-//    Misc
+    //    Misc
     func uicolorFromHex(rgbValue:UInt32)->UIColor{
         let red = CGFloat((rgbValue & 0xFF0000) >> 16)/255.0
         let green = CGFloat((rgbValue & 0xFF00) >> 8)/255.0
@@ -163,4 +210,5 @@ class MonsterTableController: UITableViewController, UISearchControllerDelegate,
         
         return UIColor(red:red, green:green, blue:blue, alpha:1.0)
     }
+    
 }
