a613f4ee6a370807d0cb933404615e471b7e11f8
Rohil Thopu <rohilthopu@gmail.com>
2019-01-31 14:37:04 -0600

    fixed bug where effect was casted to the wrong type for Enemy Skill

--- pad-db/LoadDataVC.swift
+++ pad-db/LoadDataVC.swift
@@ -362,7 +362,7 @@ class LoadDataVC: UIViewController {
                     
                     let item = NSManagedObject(entity: entity, insertInto: managedContext)
                     item.setValue(skill["name"].stringValue, forKey: "name")
-                    item.setValue(skill["effect"].intValue, forKey: "effect")
+                    item.setValue(skill["effect"].stringValue, forKey: "effect")
                     item.setValue(skill["enemy_skill_id"].intValue, forKey: "enemy_skill_id")
                     enemySkills.append(item)
                 }
