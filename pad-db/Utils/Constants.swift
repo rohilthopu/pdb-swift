75825a7e97d7b0be671eeeac70c1a801b52ce763
Rohil Thopu <rohilthopu@gmail.com>
2019-08-28 22:16:21 -0500

    just include this shit cuz who cares anymore

---
+++ pad-db/Utils/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  constants.swift
+//  pad-db
+//
+//  Created by Rohil Thopu on 1/29/19.
+//  Copyright © 2019 Rohil Thopu. All rights reserved.
+//
+
+import Foundation
+
+let api_base_link = "https://www.pad-db.com/api/"
+
+let guerrilla_api_hook = api_base_link + "guerrilla"
+let monster_list_api_hook:String = api_base_link + "monsters"
+let skills_list_api_hook:String = api_base_link + "skills"
+let dungeon_list_api_hook:String = api_base_link + "dungeons"
+let floor_list_api_hook:String = api_base_link + "floors/"
+let floor_api_hook:String = api_base_link + "floor/"
+let monster_api_url = api_base_link + "monster/"
+let skill_api_url = api_base_link + "skill/"
+let portrait_url:String = "https://pad-db.com/static/padimages/portrait/"
+let full_url:String = "https://pad-db.com/static/padimages/full/"
+let pngEngding:String = ".png"
+
+let dungeon_api_url = "https://www.pad-db.com/api/dungeons/"
+let floor_api_url = "https://www.pad-db.com/api/floors/"
